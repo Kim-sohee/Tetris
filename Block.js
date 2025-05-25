018db0faf62ccdfe934bcc2cdd95a960d6b4a2dc
sohee <soheekim980.naver.com>
2025-05-25 16:57:22 +0900

    블럭 떨어짐 및 바닥 충돌 처리

--- Block.js
+++ Block.js
@@ -2,4 +2,17 @@ class Block extends GameObject{
     constructor(container, x, y, width, height, velX, velY, color){
         super(container, x, y, width, height, velX, velY, color);
     }
+
+    //메서드 오버라이딩
+    tick(){
+        this.x += this.velX;
+        this.y += this.velY;
+    }
+
+    render(){
+        //여기서 충돌 검사?
+
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+    }
 }
